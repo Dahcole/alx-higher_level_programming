594cc4d69870a6fa2e090d12c157d3900ae118be
Dahcole <franklynnnolukachukwudalu@gmail.com>
2022-12-09 15:21:31 -0800

    file added

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,25 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    listix = []
-    listy = []
-    for x, j in my_list:
-        listix.append(x*j)
-    for x, j in my_list:
-        listy.append(j)
-    return(sum(listix)/sum(listy))
-
-
-           #second method :
-       #liste comprehension : 
-   #somme = sum([x*y for x, y in my_list])
-  #return(somme/sum([y for x, y in my_list])
-
-           #Third method :
-       #Direct additions using Tuples :
-   #     somme1, somme2 = 0
-   #     for tulpe in my_list:
-   #     somme1 +=(tulpe[0] * tulp[1])
-   #     somme2 += tulp[1]
-   #     somme1/somme2
+        if not isinstance(my_list, list) or len(my_list) == 0:
+            return 0
+        size = avg = 0
+        for tup in my_list:
+            x, y = tup
+            size += (x * y)
+            avg += y
+        return size / avg
